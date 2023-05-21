d03b43cc709aa9ce6896f337bfbb9b272542f1f2
areum0116 <areum0116@naver.com>
2023-05-21 21:15:28 +0900

    2023.5.21

--- myPage.js
+++ myPage.js
@@ -2,16 +2,16 @@ const $dropdown = $(".dropdown");
 const $dropdownToggle = $(".dropdown-toggle");
 const $dropdownMenu = $(".dropdown-menu");
 const showClass = "show";
-$(window).on("load resize", function() {
+$(window).on("load resize", function () {
   if (this.matchMedia("(min-width: 768px)").matches) {
     $dropdown.hover(
-      function() {
+      function () {
         const $this = $(this);
         $this.addClass(showClass);
         $this.find($dropdownToggle).attr("aria-expanded", "true");
         $this.find($dropdownMenu).addClass(showClass);
       },
-      function() {
+      function () {
         const $this = $(this);
         $this.removeClass(showClass);
         $this.find($dropdownToggle).attr("aria-expanded", "false");
@@ -25,3 +25,14 @@ $(window).on("load resize", function() {
 
 // 
 
+
+let date = new Date();
+const weekOfDay = ['일', '월', '화', '수', '목', '금', '토'];
+
+for (let i = 1; i <= 7; i++) {
+  for (let j = 0; j <= 1; j++) {
+    let shownDate = document.querySelectorAll(`#date${i}`)[j];
+    shownDate.textContent = date.getMonth() + 1 + '.' + date.getDate() + ' ' + weekOfDay[date.getDay()];
+  }
+  date.setDate(date.getDate() + 1);
+}
