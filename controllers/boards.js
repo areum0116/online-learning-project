310b5e333c6e4264b37a9cc96e530677c243e929
areum0116 <areum0116@naver.com>
2023-10-09 14:12:04 +0900

    2023.10.9

---
+++ controllers/boards.js
@@ -0,0 +1,50 @@
+const Board = require('../models/board');
+
+module.exports.index = async (req, res) => {
+    const boards = await Board.find({});
+    res.render('boards/index', { boards });
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('boards/new');
+}
+
+module.exports.createBoard = async (req, res, next) => {
+    const board = new Board(req.body.board);
+    board.author = req.user._id;
+    await board.save();
+    req.flash('success', '새로운 글 작성');
+    res.redirect(`/boards/${board._id}`);
+}
+
+module.exports.showBoard = async (req, res) => {
+    const board = await Board.findById(req.params.id).populate('author');
+    if (!board) {
+        req.flash('error', '해당 글은 삭제되었거나 존재하지 않습니다.');
+        return res.redirect('/boards');
+    }
+    res.render('boards/show', { board });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const board = await Board.findById(req.params.id);
+    if (!board) {
+        req.flash('error', '해당 글은 삭제되었거나 존재하지 않습니다.');
+        return res.redirect('/boards');
+    }
+    res.render('boards/edit', { board });
+}
+
+module.exports.updateBoard = async (req, res) => {
+    const { id } = req.params;
+    const board = await Board.findByIdAndUpdate(id, { ...req.body.board });
+    req.flash('success', '수정 완료');
+    res.redirect(`/boards/${board._id}`);
+}
+
+module.exports.deleteBoard = async (req, res) => {
+    const { id } = req.params;
+    await Board.findByIdAndDelete(id);
+    req.flash('success', '삭제 완료');
+    res.redirect('/boards');
+}
--- routes/boards.js
+++ routes/boards.js
@@ -1,57 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const boards = require('../controllers/boards');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateBoard } = require('../middleware');
 
 const Board = require('../models/board');
 
-router.get('/', catchAsync(async (req, res) => {
-    const boards = await Board.find({});
-    res.render('boards/index', { boards });
-}));
+router.route('/')
+    .get(catchAsync(boards.index))
+    .post(isLoggedIn, validateBoard, catchAsync(boards.createBoard));
 
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('boards/new');
-});
+router.get('/new', isLoggedIn, boards.renderNewForm);
 
-router.post('/', isLoggedIn, validateBoard, catchAsync(async (req, res, next) => {
-    const board = new Board(req.body.board);
-    board.author = req.user._id;
-    await board.save();
-    req.flash('success', '새로운 글 작성');
-    res.redirect(`/boards/${board._id}`);
-}));
+router.route('/:id')
+    .get(catchAsync(boards.showBoard))
+    .put(isLoggedIn, isAuthor, validateBoard, catchAsync(boards.updateBoard))
+    .delete(isLoggedIn, isAuthor, catchAsync(boards.deleteBoard));
 
-router.get('/:id', catchAsync(async (req, res) => {
-    const board = await Board.findById(req.params.id).populate('author');
-    if (!board) {
-        req.flash('error', '해당 글은 삭제되었거나 존재하지 않습니다.');
-        return res.redirect('/boards');
-    }
-    res.render('boards/show', { board });
-}));
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const board = await Board.findById(req.params.id);
-    if (!board) {
-        req.flash('error', '해당 글은 삭제되었거나 존재하지 않습니다.');
-        return res.redirect('/boards');
-    }
-    res.render('boards/edit', { board });
-}));
-
-router.put('/:id', isLoggedIn, isAuthor, validateBoard, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const board = await Board.findByIdAndUpdate(id, { ...req.body.board });
-    req.flash('success', '수정 완료');
-    res.redirect(`/boards/${board._id}`);
-}));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Board.findByIdAndDelete(id);
-    req.flash('success', '삭제 완료');
-    res.redirect('/boards');
-}));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(boards.renderEditForm));
 
 module.exports = router;
